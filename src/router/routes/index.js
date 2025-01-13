6478d4b7f51f92077906cc3a2dd47f5033233ea6
tuanan <pantoan288@gmail.com>
2025-01-14 01:16:46 +0700

    Add ProtectRoute for Separate Role

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -1,7 +1,11 @@
 import { privateRoutes } from './privateRoutes';
 import MainLayout from '../../layout/MainLayout.jsx';
+import ProtectRoute from './ProtectRoute.jsx';
 
 export const getRoutes = () => {
+    privateRoutes.map(r => {
+        r.element = <ProtectRoute route={r}>{r.element}</ProtectRoute>
+    })
     return {
         path: '/',
         element: <MainLayout />,
--- src/router/routes/publicRoutes.js
+++ src/router/routes/publicRoutes.js
@@ -3,6 +3,7 @@ const AdminLogin = lazy(() => import('../../views/auth/AdminLogin'));
 const Login = lazy(() => import('../../views/auth/Login'));
 const Register = lazy(() => import('../../views/auth/Register')); 
 const Home = lazy(() => import('../../views/Home'));
+const UnAuthorized = lazy(() => import('../../views/UnAuthorized'));
 
 const publicRoutes =[
     {
@@ -20,6 +21,10 @@ const publicRoutes =[
     {
         path: '/register',
         element: <Register />
+    },
+    {
+        path: 'unauthorized',
+        element: <UnAuthorized />
     }
 ]
 
