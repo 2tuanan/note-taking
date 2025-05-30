c8136a08fec7d6ea2e0bd829c0c964e120b3ee8b
tuanan <pantoan288@gmail.com>
2025-05-30 09:51:04 +0700

    Refactor MainLayout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -5,9 +5,11 @@ import Footer from './Footer';
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className='flex flex-col min-h-screen'>
             <Header />
-            <Outlet />
+            <main className='flex-grow'>
+                <Outlet />
+            </main>
             <Footer />
         </div>
     );
